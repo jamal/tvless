db476b9ad908be2bbe2718ba0a6e961e35e7dc23
jamal <jamal@yahoo-inc.com>
2016-01-10 10:37:25 -0500

    initial commit

---
+++ tvless-notifier/src/main/java/tvless/NotifierApplication.java
@@ -0,0 +1,15 @@
+package tvless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotifierApplication.class, args);
+    }
+
+}
+
+
